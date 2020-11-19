de72a3f7978b1b567149d3799073621333773d3e
florea762 <andreea97florea@gmail.com>
2020-11-19 17:38:09 +0200

    FizzBuzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,22 @@
 package com.company;
 
-public class Main {
+public class Challenge_1 {
+public static void main(String[] args){
+        System.out.println("Challenge ");
 
-    public static void main(String[] args) {
-	int[] anArray;
+        for(int i=1;i<=100;i++){
+            if(i%3!==0 && i%5! ==0){
+                System.out.println(i);
+            }
+            if(i%3==0){
+                System.out.println("fizz, ");
+            }
 
+            if(i%5==0){
+                System.out.println("buzz, ");
+            }
+
+            System.out.print(", ");
+        }
     }
 }
