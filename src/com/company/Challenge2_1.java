0056bfba33efc6f738b4e9fd78a0e1bc63ae77aa
florea762 <andreea97florea@gmail.com>
2020-11-20 19:35:08 +0200

    FooBarQix

---
+++ src/com/company/Challenge2_1.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.lang.String;
+import java.util.Scanner;
+
+public class Challenge2_1 {
+
+    static String compute(int c){
+        String str = "";
+
+        if (c % 3 == 0)
+            str = str + "Foo";
+
+        if (c % 5 == 0)
+            str = str + "Bar";
+
+        if (c % 7 == 0)
+            str = str + "Qix";
+
+        String Num = String.valueOf(c);
+
+        for (int i = 0; i < Num.length(); i++) {
+            char d=Num.charAt(i);
+            if (d == '3')
+                str += "Foo";
+
+            if (d == '5')
+                str = str + "Bar";
+
+            if (d == '7')
+                str = str + "Qix";
+        }
+
+        if (str.isEmpty())
+            return Num;
+        else
+            return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Challenge 2.1");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        String rez=compute(number);
+        System.out.println(rez);
+    }
+}
